768c8d2e6c2aba5b3874047dc297e5efa15a4a26
Robert Merget <robert.merget@tii.ae>
2023-03-17 13:50:16 +0400

    added furhgter signature algorithms

---
+++ src/main/java/de/rub/nds/protocol/constants/SignatureAlgorithm.java
@@ -0,0 +1,13 @@
+package de.rub.nds.protocol.constants;
+
+public enum SignatureAlgorithm {
+    RSA_PKCS1,
+    DSA,
+    ECDSA,
+    RSA_PSS,
+    ED25519,
+    ED448,
+    GOSTR34102001,
+    GOSTR34102012_256,
+    GOSTR34102012_512,
+}
