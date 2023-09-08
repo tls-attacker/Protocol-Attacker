c2801e004d5928b9121ccba51a59b9f06d1633f2
Robert Merget <robert.merget@tii.ae>
2023-09-08 11:50:50 +0400

    added equals and hashcode for ffdh parameters

--- src/main/java/de/rub/nds/protocol/crypto/ffdh/ExplicitFfdhGroup.java
+++ src/main/java/de/rub/nds/protocol/crypto/ffdh/ExplicitFfdhGroup.java
@@ -35,4 +35,28 @@ public class ExplicitFfdhGroup implements FfdhGoupParameters {
     public int getElementSize() {
         return modulus.bitLength();
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((modulus == null) ? 0 : modulus.hashCode());
+        result = prime * result + ((generator == null) ? 0 : generator.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        ExplicitFfdhGroup other = (ExplicitFfdhGroup) obj;
+        if (modulus == null) {
+            if (other.modulus != null) return false;
+        } else if (!modulus.equals(other.modulus)) return false;
+        if (generator == null) {
+            if (other.generator != null) return false;
+        } else if (!generator.equals(other.generator)) return false;
+        return true;
+    }
 }
