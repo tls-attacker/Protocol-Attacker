12af95272b80032f8b42715de46a15d9e1bf0c3f
Robert Merget <robert.merget@tii.ae>
2024-09-27 09:35:46 +0400

    added more mac algorithms

--- src/main/java/de/rub/nds/protocol/constants/MacAlgorithm.java
+++ src/main/java/de/rub/nds/protocol/constants/MacAlgorithm.java
@@ -11,24 +11,29 @@ package de.rub.nds.protocol.constants;
 /** Metadata for hash algorithms. */
 public enum MacAlgorithm {
     NONE(0, 0, null),
-    HMAC_MD5(128, 64, "HmacMD5"),
-    HMAC_SHA1(160, 80, "HmacSHA1"),
-    HMAC_SHA256(256, 128, "HmacSHA256"),
-    HMAC_SHA384(384, 192, "HmacSHA384"),
-    HMAC_SHA512(512, 256, "HmacSHA512"),
-    HMAC_SHA512_224(224, 112, "HmacSHA512/224"),
-    HMAC_SHA512_256(256, 128, "HmacSHA512/256");
+    HMAC_MD5(16, 16, "HmacMD5"),
+    HMAC_SHA1(20, 20, "HmacSHA1"),
+    HMAC_SHA256(32, 32, "HmacSHA256"),
+    HMAC_SHA384(48, 48, "HmacSHA384"),
+    HMAC_SHA512(64, 64, "HmacSHA512"),
+    HMAC_SHA512_224(28, 28, "HmacSHA512/224"),
+    HMAC_SHA512_256(32, 32, "HmacSHA512/256"),
+    IMIT_GOST28147(4, 32, "GOST28147MAC"),
+    HMAC_GOSTR3411(32, 32, "HmacGOST3411"),
+    HMAC_GOSTR3411_2012_256(32, 32, "HmacGOST3411-2012-256"),
+    HMAC_SM3(32, 32, "HmacSM3");
 
-    /** The length of a hash */
-    private int bitLength;
+    /** The length of a MAC in byte */
+    private int macLength;
 
-    private int securityStrength;
+    /** The lenght of a MAC in byte */
+    private int keySize;
 
     private String javaName;
 
-    private MacAlgorithm(int bitStrength, int securityStrength, String javaName) {
-        this.bitLength = bitStrength;
-        this.securityStrength = securityStrength;
+    private MacAlgorithm(int macLength, int keySize, String javaName) {
+        this.macLength = macLength;
+        this.keySize = keySize;
         this.javaName = javaName;
     }
 
@@ -36,11 +41,11 @@ public enum MacAlgorithm {
         return javaName;
     }
 
-    public int getBitLength() {
-        return bitLength;
+    public int getMacLength() {
+        return macLength;
     }
 
-    public int getSecurityStrength() {
-        return securityStrength;
+    public int getKeySize() {
+        return keySize;
     }
 }
