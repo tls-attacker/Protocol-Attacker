aaf06c7e1273e8544872376814e99bbd4dec3faa
mmaehren <32199075+mmaehren@users.noreply.github.com>
2024-10-24 08:59:19 +0200

    Use previous PSS code for all PSS signatures. Deleted mask hash algorithm parameter

--- src/main/java/de/rub/nds/protocol/crypto/signature/SignatureCalculator.java
+++ src/main/java/de/rub/nds/protocol/crypto/signature/SignatureCalculator.java
@@ -23,7 +23,6 @@ import de.rub.nds.protocol.exception.CryptoException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.Arrays;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -62,8 +61,7 @@ public class SignatureCalculator {
                     (RsaPrivateKey) privateKey,
                     toBeSignedBytes,
                     hashAlgorithm,
-                    ((RsaSsaPssSignatureComputations) computations).getSalt().getValue(),
-                    HashAlgorithm.SHA1);
+                    ((RsaSsaPssSignatureComputations) computations).getSalt().getValue());
         } else if (computations instanceof RsaPkcs1SignatureComputations) {
             // Check That parameters are compatible
             if (!(privateKey instanceof RsaPrivateKey)) {
@@ -72,17 +70,6 @@ public class SignatureCalculator {
             }
             computeRsaPkcs1Signature(
                     (RsaPkcs1SignatureComputations) computations,
-                    (RsaPrivateKey) privateKey,
-                    toBeSignedBytes,
-                    hashAlgorithm);
-        } else if (computations instanceof RsaPssRsaeSignatureComputations) {
-            // Check That parameters are compatible
-            if (!(privateKey instanceof RsaPrivateKey)) {
-                throw new IllegalArgumentException(
-                        "RSA RSAE SignatureComputations must be used with a RSA PrivateKey");
-            }
-            computeRsaPssRsaeSignature(
-                    (RsaPssRsaeSignatureComputations) computations,
                     (RsaPrivateKey) privateKey,
                     toBeSignedBytes,
                     hashAlgorithm);
@@ -141,8 +128,7 @@ public class SignatureCalculator {
             RsaPrivateKey privateKey,
             byte[] toBeSignedBytes,
             HashAlgorithm hashAlgorithm,
-            byte[] salt,
-            HashAlgorithm mgf1Algorithm) {
+            byte[] salt) {
         LOGGER.trace("Computing RSA-PSS signature");
 
         computations.setPrivateKey(privateKey.getPrivateExponent());
@@ -191,120 +177,24 @@ public class SignatureCalculator {
         db = computations.getDbValue().getValue();
         LOGGER.debug("DB: {}", db);
         // Mask generation function (MGF1)
-        byte[] dbMask = maskGeneratorFunction1(hValue, mgf1Algorithm, emLength - hValue.length - 1);
-        LOGGER.debug("DB mask: {}", dbMask);
-        byte[] maskedDB = mask(db, dbMask);
-        computations.setMaskedDb(maskedDB);
-        maskedDB = computations.getMaskedDb().getValue();
-        LOGGER.debug("Masked DB: {}", maskedDB);
-        computations.setTfValue(new byte[] {(byte) 0xBC});
-
-        int firstByteMask = 0xff >>> ((emLength * 8) - emBits);
-        if (maskedDB.length > 0) {
-            maskedDB[0] &= firstByteMask;
-        }
-        // Construct the encoded message EM = maskedDB || H || 0xBC
-        byte[] em =
-                ArrayConverter.concatenate(maskedDB, hValue, computations.getTfValue().getValue());
-        computations.setEmValue(em);
-        em = computations.getEmValue().getValue();
-        LOGGER.debug("EM: {}", em);
-        // Convert EM to an integer
-        BigInteger emInteger = new BigInteger(1, em);
-
-        // Signature calculation: s = (emInteger^d) mod n
-        BigInteger signature =
-                emInteger.modPow(
-                        computations.getPrivateKey().getValue(),
-                        computations.getModulus().getValue());
-        computations.setSignatureBytes(ArrayConverter.bigIntegerToByteArray(signature));
-        computations.setSignatureValid(true);
-    }
-
-    public void computeRsaPssRsaeSignature(
-            RsaPssRsaeSignatureComputations computations,
-            RsaPrivateKey privateKey,
-            byte[] toBeSignedBytes,
-            HashAlgorithm hashAlgorithm) {
-        LOGGER.trace("Computing RSA-PSS-RSAE signature");
-
-        computations.setPrivateKey(privateKey.getPrivateExponent());
-        computations.setModulus(privateKey.getModulus());
-        computations.setToBeSignedBytes(toBeSignedBytes);
-        computations.setHashAlgorithm(hashAlgorithm);
-
-        // Hash the message
-        byte[] digest =
-                HashCalculator.compute(computations.getToBeSignedBytes().getValue(), hashAlgorithm);
-        computations.setDigestBytes(digest);
-        digest = computations.getDigestBytes().getValue();
-        LOGGER.debug("Digest: {}", digest);
-
-        // Generate a random salt of length equal to the hash output length
-        int hashOutputLength = hashAlgorithm.getBitLength() / 8;
-        byte[] salt = new byte[hashOutputLength];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(salt);
-        computations.setSalt(salt);
-        LOGGER.debug("Salt: {}", salt);
-
-        // M' = (0x)00 00 00 00 00 00 00 00 || mHash || salt
-        byte[] paddedSaltedDigest =
-                ArrayConverter.concatenate(new byte[8], digest, computations.getSalt().getValue());
-        computations.setPaddedSaltedDigest(paddedSaltedDigest);
-        paddedSaltedDigest = computations.getPaddedSaltedDigest().getValue();
-        LOGGER.debug("Padded salted digest: {}", paddedSaltedDigest);
-
-        // Hash M' to get H
-        byte[] hValue = HashCalculator.compute(paddedSaltedDigest, hashAlgorithm);
-        computations.setHValue(hValue);
-        hValue = computations.getHValue().getValue();
-        LOGGER.debug("H: {}", hValue);
-
-        // Generate padding string PS, which is a string of zero bytes
-        int emBits = computations.getModulus().getValue().bitLength() - 1;
-        int emLength = (emBits + 7) / 8;
-        int psLength = emLength - computations.getSalt().getValue().length - hValue.length - 2;
-        if (psLength < 0) {
-            LOGGER.warn("PS length is negative. Overwriting with 0");
-            psLength = 0;
-        }
-        byte[] psValue = new byte[psLength];
-        computations.setPsValue(psValue);
-        psValue = computations.getPsValue().getValue();
-        LOGGER.debug("PS value: {}", psValue);
-
-        // Generate the DB = PS || 0x01 || salt
-        byte[] db =
-                ArrayConverter.concatenate(
-                        psValue, new byte[] {0x01}, computations.getSalt().getValue());
-        computations.setDbValue(db);
-        db = computations.getDbValue().getValue();
-        LOGGER.debug("DB: {}", db);
-
-        // Mask generation function (MGF1) using the same hash algorithm
         byte[] dbMask = maskGeneratorFunction1(hValue, hashAlgorithm, emLength - hValue.length - 1);
         LOGGER.debug("DB mask: {}", dbMask);
-
         byte[] maskedDB = mask(db, dbMask);
         computations.setMaskedDb(maskedDB);
         maskedDB = computations.getMaskedDb().getValue();
         LOGGER.debug("Masked DB: {}", maskedDB);
-
         computations.setTfValue(new byte[] {(byte) 0xBC});
 
         int firstByteMask = 0xff >>> ((emLength * 8) - emBits);
         if (maskedDB.length > 0) {
             maskedDB[0] &= firstByteMask;
         }
-
         // Construct the encoded message EM = maskedDB || H || 0xBC
         byte[] em =
                 ArrayConverter.concatenate(maskedDB, hValue, computations.getTfValue().getValue());
         computations.setEmValue(em);
         em = computations.getEmValue().getValue();
         LOGGER.debug("EM: {}", em);
-
         // Convert EM to an integer
         BigInteger emInteger = new BigInteger(1, em);
 
@@ -731,7 +621,6 @@ public class SignatureCalculator {
             case RSA_PKCS1:
                 return new RsaPkcs1SignatureComputations();
             case RSA_PSS_RSAE:
-                return new RsaPssRsaeSignatureComputations();
             case RSA_SSA_PSS:
                 return new RsaSsaPssSignatureComputations();
             default:
