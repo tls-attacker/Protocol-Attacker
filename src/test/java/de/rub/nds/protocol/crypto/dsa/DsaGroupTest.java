19b3131b1f45ece5b47daa0fc35100c58f938606
Robert Merget <robert.merget@tii.ae>
2025-03-24 14:13:40 +0400

    removed unnecessary ;

--- src/test/java/de/rub/nds/protocol/crypto/dsa/DsaGroupTest.java
+++ src/test/java/de/rub/nds/protocol/crypto/dsa/DsaGroupTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DsaGroupTest {
-    ;
+    
     private ExplicitDsaParameters explicitParameters;
 
     @BeforeEach
