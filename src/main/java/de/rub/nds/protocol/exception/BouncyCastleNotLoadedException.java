806489a2186651e1c2c29f995304f2fbe8307b54
Robert Merget <robert.merget@tii.ae>
2025-06-19 17:51:56 +0000

    Add Javadoc to BouncyCastleNotLoadedException constructors

--- src/main/java/de/rub/nds/protocol/exception/BouncyCastleNotLoadedException.java
+++ src/main/java/de/rub/nds/protocol/exception/BouncyCastleNotLoadedException.java
@@ -9,14 +9,26 @@
 package de.rub.nds.protocol.exception;
 
 public class BouncyCastleNotLoadedException extends RuntimeException {
+    /** Constructs a new BouncyCastleNotLoadedException with no detail message. */
     public BouncyCastleNotLoadedException() {
         super();
     }
 
+    /**
+     * Constructs a new BouncyCastleNotLoadedException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public BouncyCastleNotLoadedException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new BouncyCastleNotLoadedException with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
     public BouncyCastleNotLoadedException(String message, Throwable cause) {
         super(message, cause);
     }
