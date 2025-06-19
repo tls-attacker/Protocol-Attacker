ebf6054536dcf032fb88f2b8bfc1a336974ca1af
Robert Merget <robert.merget@tii.ae>
2025-06-19 18:07:36 +0000

    Add comprehensive unit tests for DsaParameters to achieve 100% coverage
    - Added tests for equals() method with null fields
    - Added tests for hashCode() method with null fields
    - Added specific tests for different q and g values in equals()
    - Achieved 100% instruction and branch coverage for DsaParameters class

--- src/test/java/de/rub/nds/protocol/constants/DsaParametersTest.java
+++ src/test/java/de/rub/nds/protocol/constants/DsaParametersTest.java
@@ -101,4 +101,113 @@ class DsaParametersTest {
                 params1.hashCode(),
                 params3.hashCode()); // Different objects may have different hash codes
     }
+
+    @Test
+    void testEqualsWithNullFields() {
+        // Test parameters with null p
+        DsaParameters paramsWithNullP =
+                new TestDsaParameters(null, BigInteger.valueOf(11), BigInteger.valueOf(2));
+        DsaParameters paramsWithNullP2 =
+                new TestDsaParameters(null, BigInteger.valueOf(11), BigInteger.valueOf(2));
+        DsaParameters paramsWithNonNullP =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(2));
+
+        // Test equals with null p
+        assertTrue(paramsWithNullP.equals(paramsWithNullP2));
+        assertFalse(paramsWithNullP.equals(paramsWithNonNullP));
+        assertFalse(paramsWithNonNullP.equals(paramsWithNullP));
+
+        // Test parameters with null q
+        DsaParameters paramsWithNullQ =
+                new TestDsaParameters(BigInteger.valueOf(23), null, BigInteger.valueOf(2));
+        DsaParameters paramsWithNullQ2 =
+                new TestDsaParameters(BigInteger.valueOf(23), null, BigInteger.valueOf(2));
+        DsaParameters paramsWithNonNullQ =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(2));
+
+        // Test equals with null q
+        assertTrue(paramsWithNullQ.equals(paramsWithNullQ2));
+        assertFalse(paramsWithNullQ.equals(paramsWithNonNullQ));
+        assertFalse(paramsWithNonNullQ.equals(paramsWithNullQ));
+
+        // Test parameters with null g
+        DsaParameters paramsWithNullG =
+                new TestDsaParameters(BigInteger.valueOf(23), BigInteger.valueOf(11), null);
+        DsaParameters paramsWithNullG2 =
+                new TestDsaParameters(BigInteger.valueOf(23), BigInteger.valueOf(11), null);
+        DsaParameters paramsWithNonNullG =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(2));
+
+        // Test equals with null g
+        assertTrue(paramsWithNullG.equals(paramsWithNullG2));
+        assertFalse(paramsWithNullG.equals(paramsWithNonNullG));
+        assertFalse(paramsWithNonNullG.equals(paramsWithNullG));
+
+        // Test parameters with all nulls
+        DsaParameters paramsAllNull = new TestDsaParameters(null, null, null);
+        DsaParameters paramsAllNull2 = new TestDsaParameters(null, null, null);
+
+        assertTrue(paramsAllNull.equals(paramsAllNull2));
+        assertFalse(paramsAllNull.equals(paramsWithNonNullP));
+    }
+
+    @Test
+    void testHashCodeWithNullFields() {
+        // Test hashCode with null p
+        DsaParameters paramsWithNullP =
+                new TestDsaParameters(null, BigInteger.valueOf(11), BigInteger.valueOf(2));
+        DsaParameters paramsWithNullP2 =
+                new TestDsaParameters(null, BigInteger.valueOf(11), BigInteger.valueOf(2));
+        assertEquals(paramsWithNullP.hashCode(), paramsWithNullP2.hashCode());
+
+        // Test hashCode with null q
+        DsaParameters paramsWithNullQ =
+                new TestDsaParameters(BigInteger.valueOf(23), null, BigInteger.valueOf(2));
+        DsaParameters paramsWithNullQ2 =
+                new TestDsaParameters(BigInteger.valueOf(23), null, BigInteger.valueOf(2));
+        assertEquals(paramsWithNullQ.hashCode(), paramsWithNullQ2.hashCode());
+
+        // Test hashCode with null g
+        DsaParameters paramsWithNullG =
+                new TestDsaParameters(BigInteger.valueOf(23), BigInteger.valueOf(11), null);
+        DsaParameters paramsWithNullG2 =
+                new TestDsaParameters(BigInteger.valueOf(23), BigInteger.valueOf(11), null);
+        assertEquals(paramsWithNullG.hashCode(), paramsWithNullG2.hashCode());
+
+        // Test hashCode with all nulls
+        DsaParameters paramsAllNull = new TestDsaParameters(null, null, null);
+        DsaParameters paramsAllNull2 = new TestDsaParameters(null, null, null);
+        assertEquals(paramsAllNull.hashCode(), paramsAllNull2.hashCode());
+    }
+
+    @Test
+    void testEqualsDifferentQ() {
+        // Test specifically for the branch where q differs
+        DsaParameters params1 =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(2));
+        DsaParameters params2 =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(5), BigInteger.valueOf(2));
+
+        assertFalse(params1.equals(params2));
+        assertFalse(params2.equals(params1));
+    }
+
+    @Test
+    void testEqualsDifferentG() {
+        // Test specifically for the branch where g differs
+        DsaParameters params1 =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(2));
+        DsaParameters params2 =
+                new TestDsaParameters(
+                        BigInteger.valueOf(23), BigInteger.valueOf(11), BigInteger.valueOf(3));
+
+        assertFalse(params1.equals(params2));
+        assertFalse(params2.equals(params1));
+    }
 }
