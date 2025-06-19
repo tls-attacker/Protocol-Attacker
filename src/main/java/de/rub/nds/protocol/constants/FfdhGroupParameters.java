7f32329a34f01b5aaf7bbd81d8840a8e3a6f9b61
Robert Merget <robert.merget@tii.ae>
2025-06-19 17:08:57 +0000

    Add Javadoc to FfdhGroupParameters public methods

--- src/main/java/de/rub/nds/protocol/constants/FfdhGroupParameters.java
+++ src/main/java/de/rub/nds/protocol/constants/FfdhGroupParameters.java
@@ -17,29 +17,60 @@ public abstract class FfdhGroupParameters implements GroupParameters<BigInteger>
     private final BigInteger generator;
     private final BigInteger modulus;
 
+    /**
+     * Constructs a new FfdhGroupParameters instance with the specified generator and modulus.
+     *
+     * @param generator the generator of the finite field Diffie-Hellman group
+     * @param modulus the modulus (prime) of the finite field Diffie-Hellman group
+     */
     public FfdhGroupParameters(BigInteger generator, BigInteger modulus) {
         this.generator = generator;
         this.modulus = modulus;
     }
 
+    /**
+     * Returns the generator of this finite field Diffie-Hellman group.
+     *
+     * @return the generator
+     */
     public BigInteger getGenerator() {
         return generator;
     }
 
+    /**
+     * Returns the modulus (prime) of this finite field Diffie-Hellman group.
+     *
+     * @return the modulus
+     */
     public BigInteger getModulus() {
         return modulus;
     }
 
+    /**
+     * Returns the size of group elements in bits.
+     *
+     * @return the bit length of the modulus
+     */
     @Override
     public int getElementSizeBits() {
         return modulus.bitLength();
     }
 
+    /**
+     * Returns the size of group elements in bytes.
+     *
+     * @return the byte length required to represent group elements
+     */
     @Override
     public int getElementSizeBytes() {
         return (int) Math.ceil(((double) getElementSizeBits()) / 8);
     }
 
+    /**
+     * Returns the cyclic group associated with these FFDH parameters.
+     *
+     * @return a FfdhGroup instance with these parameters
+     */
     @Override
     public CyclicGroup<BigInteger> getGroup() {
         return new FfdhGroup(this);
