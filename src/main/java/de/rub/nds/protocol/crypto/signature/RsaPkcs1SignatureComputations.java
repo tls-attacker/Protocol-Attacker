abeda5d0a5096975e6577f2273f00f13b3f79d72
Robert Merget <robert.merget@tii.ae>
2025-06-19 17:49:48 +0000

    Add Javadoc to RsaPkcs1SignatureComputations public methods

--- src/main/java/de/rub/nds/protocol/crypto/signature/RsaPkcs1SignatureComputations.java
+++ src/main/java/de/rub/nds/protocol/crypto/signature/RsaPkcs1SignatureComputations.java
@@ -28,70 +28,151 @@ public class RsaPkcs1SignatureComputations extends SignatureComputations {
 
     private HashAlgorithm hashAlgorithm;
 
+    /** Constructs a new RsaPkcs1SignatureComputations instance. */
     public RsaPkcs1SignatureComputations() {}
 
+    /**
+     * Gets the RSA private key used for PKCS#1 signature generation.
+     *
+     * @return the private key
+     */
     public ModifiableBigInteger getPrivateKey() {
         return privateKey;
     }
 
+    /**
+     * Sets the RSA private key used for PKCS#1 signature generation.
+     *
+     * @param privateKey the private key to set
+     */
     public void setPrivateKey(ModifiableBigInteger privateKey) {
         this.privateKey = privateKey;
     }
 
+    /**
+     * Sets the RSA private key used for PKCS#1 signature generation.
+     *
+     * @param privateKey the private key to set as BigInteger
+     */
     public void setPrivateKey(BigInteger privateKey) {
         this.privateKey = ModifiableVariableFactory.safelySetValue(this.privateKey, privateKey);
     }
 
+    /**
+     * Gets the RSA modulus.
+     *
+     * @return the RSA modulus
+     */
     public ModifiableBigInteger getModulus() {
         return modulus;
     }
 
+    /**
+     * Sets the RSA modulus.
+     *
+     * @param modulus the RSA modulus to set
+     */
     public void setModulus(ModifiableBigInteger modulus) {
         this.modulus = modulus;
     }
 
+    /**
+     * Sets the RSA modulus.
+     *
+     * @param modulus the RSA modulus to set as BigInteger
+     */
     public void setModulus(BigInteger modulus) {
         this.modulus = ModifiableVariableFactory.safelySetValue(this.modulus, modulus);
     }
 
+    /**
+     * Gets the PKCS#1 padding bytes.
+     *
+     * @return the padding bytes
+     */
     public ModifiableByteArray getPadding() {
         return padding;
     }
 
+    /**
+     * Sets the PKCS#1 padding bytes.
+     *
+     * @param padding the padding bytes to set
+     */
     public void setPadding(ModifiableByteArray padding) {
         this.padding = padding;
     }
 
+    /**
+     * Sets the PKCS#1 padding bytes.
+     *
+     * @param padding the padding bytes to set as byte array
+     */
     public void setPadding(byte[] padding) {
         this.padding = ModifiableVariableFactory.safelySetValue(this.padding, padding);
     }
 
+    /**
+     * Gets the plain data to be signed.
+     *
+     * @return the plain data to be signed
+     */
     public ModifiableByteArray getPlainToBeSigned() {
         return plainToBeSigned;
     }
 
+    /**
+     * Sets the plain data to be signed.
+     *
+     * @param plainToBeSigned the plain data to be signed
+     */
     public void setPlainToBeSigned(ModifiableByteArray plainToBeSigned) {
         this.plainToBeSigned = plainToBeSigned;
     }
 
+    /**
+     * Sets the plain data to be signed.
+     *
+     * @param plainToBeSigned the plain data to be signed as byte array
+     */
     public void setPlainToBeSigned(byte[] plainToBeSigned) {
         this.plainToBeSigned =
                 ModifiableVariableFactory.safelySetValue(this.plainToBeSigned, plainToBeSigned);
     }
 
+    /**
+     * Gets the DER-encoded digest value.
+     *
+     * @return the DER-encoded digest
+     */
     public ModifiableByteArray getDerEncodedDigest() {
         return derEncodedDigest;
     }
 
+    /**
+     * Sets the DER-encoded digest value.
+     *
+     * @param derEncodedDigest the DER-encoded digest to set
+     */
     public void setDerEncodedDigest(byte[] derEncodedDigest) {
         this.derEncodedDigest =
                 ModifiableVariableFactory.safelySetValue(this.derEncodedDigest, derEncodedDigest);
     }
 
+    /**
+     * Gets the hash algorithm used for PKCS#1 signature computation.
+     *
+     * @return the hash algorithm
+     */
     public HashAlgorithm getHashAlgorithm() {
         return hashAlgorithm;
     }
 
+    /**
+     * Sets the hash algorithm used for PKCS#1 signature computation.
+     *
+     * @param hashAlgorithm the hash algorithm to set
+     */
     public void setHashAlgorithm(HashAlgorithm hashAlgorithm) {
         this.hashAlgorithm = hashAlgorithm;
     }
