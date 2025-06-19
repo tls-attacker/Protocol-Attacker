ea70afd468ce9408ad952c6f5e44bb32d57bacef
Robert Merget <robert.merget@tii.ae>
2025-06-19 17:10:46 +0000

    Add Javadoc to IHashAlgorithm interface methods

--- src/main/java/de/rub/nds/protocol/constants/IHashAlgorithm.java
+++ src/main/java/de/rub/nds/protocol/constants/IHashAlgorithm.java
@@ -10,9 +10,25 @@ package de.rub.nds.protocol.constants;
 
 public interface IHashAlgorithm {
 
+    /**
+     * Returns the output length of this hash algorithm in bits.
+     *
+     * @return the bit length of the hash output
+     */
     int getBitLength();
 
+    /**
+     * Returns the security strength of this hash algorithm in bits.
+     *
+     * @return the security strength in bits
+     */
     int getSecurityStrength();
 
+    /**
+     * Computes the hash of the given data.
+     *
+     * @param data the input data to hash
+     * @return the computed hash value
+     */
     byte[] computeHash(byte[] data);
 }
