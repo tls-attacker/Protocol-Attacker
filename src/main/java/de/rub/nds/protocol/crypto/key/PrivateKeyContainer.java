227f55191da904609b222e4aa16ab0df52910fce
Fabian Bäumer <fabian.baeumer@rub.de>
2025-07-08 17:32:56 +0200

    docs: Remove unnecessary comment on PrivateKeyContainer interface

--- src/main/java/de/rub/nds/protocol/crypto/key/PrivateKeyContainer.java
+++ src/main/java/de/rub/nds/protocol/crypto/key/PrivateKeyContainer.java
@@ -11,8 +11,7 @@ package de.rub.nds.protocol.crypto.key;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
- * Marker interface for containers holding private key data. Implementations should securely store
- * private key material.
+ * Marker interface for containers holding private key data.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public interface PrivateKeyContainer {}
